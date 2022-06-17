3b8816db434103c525de936e4f05941c1501d960
Yakouttkadiri <57764694+Yakouttkadiri@users.noreply.github.com>
2022-06-17 11:26:28 +0200

    Update tp_devops.py

--- tp_devops.py
+++ tp_devops.py
@@ -1,16 +1,37 @@
 import requests
 import json
+from flask import Flask,  request, jsonify
 import os
-
-LAT = os.environ['LAT']
-LONG = os.environ['LONG']
+app = Flask(__name__)
 api_key = os.environ['API_KEY']
 
-def get_weather(lat, lon, api_key):
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (LAT, LONG, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data
+
+with app.app_context():
+    @app.route("/")
+    def meteo():
+        lat = request.args.get('lat')
+        lon = request.args.get('lon')
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&units=metric" % (
+            lat, lon, api_key)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        if response.status_code != 200:
+            return jsonify({
+                'status': 'error',
+                'message': 'La requête à l\'API météo n\'a pas fonctionné. Voici le message renvoyé par l\'API : {}'.format(data)
+            }), 500
+
+        return jsonify({
+            'status': 'ok',
+            'data': data
+        })
+
 
 if __name__ == "__main__":
-    print(get_weather(LAT, LONG, api_key))
+    port=80
+    app.run(host="0.0.0.0", port=port)
+
+
+
+
+
